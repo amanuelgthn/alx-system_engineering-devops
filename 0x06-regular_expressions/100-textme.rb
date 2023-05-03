1d062b42133f3c7723068591b16fdf861d60abef
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-03 10:46:05 +0800

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #The regular expression must be only matching: capital letters
-Retrieve = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
-puts [Retrieve[0].compact, Retrieve[1].compact, Retrieve[2].compact].join(',')
+fields = ARGV[0].scan(/(?<=from:|to:|flags:)(.*?)(?=\s|$)/)
+puts fields.compact.join(',')
