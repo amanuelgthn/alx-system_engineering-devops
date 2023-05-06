d57533333ddc4d06f9d5ef04b75f2d0184bfa66c
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-06 10:35:06 +0800

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 #The regular expression must be only matching: capital letters
 scrap = ARGV[0].scan(/(?<=from:|to:|flags:)(.*?)(?=\s|$)/)
 fields = [scrap[0],(scrap[1],scrap[2]]
-echo fields.join(',')
+puts fields.join(',')
