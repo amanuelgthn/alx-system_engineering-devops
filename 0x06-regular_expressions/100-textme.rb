45a7cb3fcaf7e4d915027c17d4d3f5a04f322929
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-06 10:37:29 +0800

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 #The regular expression must be only matching: capital letters
-scrap = ARGV[0].scan(/(?<=from:|to:|flags:)(.*?)(?=\s|$)/)
-fields = [scrap[0],(scrap[1],scrap[2]]
+scrap = ARGV[0].scan(/(?<=from:|to:|flags:)(.*?)]/)
+fields = [scrap[0],scrap[1],scrap[2]]
 puts fields.join(',')
