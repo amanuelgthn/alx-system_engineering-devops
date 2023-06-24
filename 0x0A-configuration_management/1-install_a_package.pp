bdbe3ba5f9370f44e69d0f838a41f27050f13e2e
amanuelgthn <amanuelgthn@gmail.com>
2023-06-24 10:13:43 +0800

    Create 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Install flask from pip3
 # version must be 2.1.0
 package {'flask':
-    ensure   => present,
-    provides => pip3,
-    version  => '2.1.0',
+    ensure   => '2.1.0',
+    provider => 'pip3',
     }
