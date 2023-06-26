bd574512c7bfd1ac183446e2a153016d991bd86d
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-26 10:23:27 +0800

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,5 @@
-#!/usr/bin/env bash
+#configuration using a puppet file
+exec { 'configure-ssh-client':
+  command => 'echo "Host *\n  IdentityFile ~/.ssh/school\n  PassWordAuthentication no" >> ~/.ssh/config',
+  path => '/usr/bin:/bin',
+}
