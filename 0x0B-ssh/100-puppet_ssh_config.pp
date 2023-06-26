a3bb5d85580fc7864e5938810d9d5f85d2ebc457
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-26 10:37:39 +0800

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 #configuration using a puppet file
 exec { 'ssh_config':
   path => '/bin',
-  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',}
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
   }
