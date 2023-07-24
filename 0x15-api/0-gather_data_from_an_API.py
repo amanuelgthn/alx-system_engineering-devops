c4e3615f2bb40fb70412758400f5d518e7ab8297
amanuelgthn <amanuelgthn@gmail.com>
2023-07-24 20:46:32 +0800

    Create 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Python script that using RESTAPI for a given employee ID,returns information
+about his/her TODO list progress
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users'
+    refined_url_name = '{}/{}'.format(url, id)
+    refined_url_todo = '{}/todos'.format(refined_url_name)
+    response = requests.get(refined_url_name)
+    response_todos = requests.get(refined_url_todo)
+    json_response_name = response.json()
+    json_response_todo = response_todos.json()
+    employee_name = json_response_name["name"]
+    i = 0
+    j = 0
+    tasks = []
+    for obj in json_response_todo:
+        if obj["completed"] is True:
+            i += 1
+            tasks.append(obj["title"])
+        j += 1
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, i, j))
+    for task in tasks:
+        print("\t{}".format(task))
