7686ab1575aa29e5114a51bd44717c44ec483008
amanuelgthn <amanuelgthn@gmail.com>
2023-08-09 18:35:25 +0800

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,15 @@ Python script that exports the information from REST API to json file
 """
 
 
-if __name__ == '__main__':
-    import json
-    import requests
-    from sys import argv
+import json
+import requests
+from sys import argv
 
-    def number_of_subscribers(subreddit):
-        url = 'https://www.reddit.com/r/{}/about.json'.format(argv[2])
-        response = requests.get(url)
-        json_response_name = response.json()
-        print(json_response_name)
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url)
+    json_response_name = response.json()
+    try:
+        return json_response_name["data"]["subscribers"]
+    except Exception:
+        return 0
