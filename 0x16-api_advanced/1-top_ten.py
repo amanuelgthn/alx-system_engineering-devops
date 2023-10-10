ed255cb94aa98c276f4cc5bda7036ca99d5fda26
amanuelgthn <amanuelgthn@gmail.com>
2023-10-10 15:03:57 +0200

    Update

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that exports the information from REST API to json file
+"""
+
+
+import json
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url=url,
+                            headers=headers,
+                            allow_redirects=False).json()
+    for i in range(10):
+        try:
+            data = response['data']['children'][i]
+            print(data['data']['title'])
+        except Exception:
+            print(None)
+            return
