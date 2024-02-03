92fe08728e79410d7e79c052c90989c692ff0048
amanuelgthn <amanuelgthn@gmail.com>
2024-02-03 15:12:44 +0100

    update csv-writer quoting=csv.QUOTE_ALL

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     json_response_todo = response_todos.json()
     user_name = json_response_name["username"]
     with open(file_name, 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for obj in json_response_todo:
             row = []
             row.append('{}'.format(obj["userId"]))
